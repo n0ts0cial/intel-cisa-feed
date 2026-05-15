342654795db3b0bc43ce0944da6ddd1390fff88c
n0ts0cial <121235487+n0ts0cial@users.noreply.github.com>
2026-05-15 01:39:18 -0300

    Update intel-cisa-feed.ps1

--- intel-cisa-feed.ps1
+++ intel-cisa-feed.ps1
@@ -121,3 +121,4 @@ function Get-SpecificDateVulnerabilities {
 
 # --- EXECUTION ---
 Get-SpecificDateVulnerabilities -TargetDate "2024-05-14"
+Get-SpecificDateVulnerabilities -TargetDate "2026-05-15"
